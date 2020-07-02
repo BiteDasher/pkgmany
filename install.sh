63825dce332225a1276311dc9cb70794d0a64b6e
BiteDasher <cazorax@mail.ru>
2020-07-02 21:27:35 +0300

    add remove check in install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,4 @@ else
 	sudo install -m 755 pkgmany /usr/local/bin/pkgmany
 	sudo mkdir -p /usr/share/debmany
 fi
-sudo apt install coreutils bash sed gdebi git curl wget
+! [ "$1" == "remove" ] && sudo apt install coreutils bash sed gdebi git curl wget
